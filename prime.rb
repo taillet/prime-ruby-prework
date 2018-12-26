8f1a67bb1b17c481dfc9369585d328f25545a5c3
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 17:06:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num == 3
+    return true
+  else
+    for e in 2..Math.sqrt(num)
+      if num % e != 0
+        return true
+      elsif num % e == 0
+        return false
+      end
+    end
+  end
+end
